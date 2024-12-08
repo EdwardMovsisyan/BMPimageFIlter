4b8370d6b2073cbdfe2f02917746b21a973d1179
Edward <edvardmovs@gmail.com>
2024-12-09 00:10:29 +0400

    adding cmake to project

---
+++ CMakeFiles/custom_clean.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Makefile"
+  "Output"
+  "CMakeFiles/custom_clean"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/custom_clean.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Makefile"
+  "Output"
+  "CMakeFiles/main"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
